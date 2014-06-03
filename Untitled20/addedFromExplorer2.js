9e8f75f0f73c6fe7a424639d58dcef3979073c54
Ismail Bougnouch <ismail.bougnouch@4D.com>
2014-06-03 14:43:09 +0200

    Update #11

---
+++ Untitled20/addedFromExplorer2.js
@@ -0,0 +1,26 @@
+ Le volume dans le lecteur C n'a pas de nom.
+ Le num‚ro de s‚rie du volume est 606E-C054
+
+ R‚pertoire de C:\Users\Ismail\Documents\Wakanda\Untitled20\Untitled20
+
+03/06/2014  14:42    <DIR>          .
+03/06/2014  14:42    <DIR>          ..
+03/06/2014  14:39             1ÿ182 addedFromExplorer.js
+03/06/2014  14:42                 0 addedFromExplorer2.js
+03/06/2014  11:35    <DIR>          DataFolder
+17/04/2014  12:11               267 Model.waModel
+03/06/2014  12:21                21 newFile.js
+17/04/2014  12:11                68 Permissions.waPerm
+03/06/2014  11:35             1ÿ645 Settings.waSettings
+03/06/2014  14:41               344 SymbolTable.Match
+03/06/2014  14:41           197ÿ120 SymbolTable.waSym
+03/06/2014  14:41         6ÿ357ÿ504 SymbolTable.waSymData
+03/06/2014  11:35    <DIR>          Themes
+03/06/2014  12:21                40 toChange.js
+03/06/2014  12:25                15 toCheck.js
+03/06/2014  12:07                14 toDelete.js
+17/04/2014  12:11               578 Untitled20.waProject
+03/06/2014  11:35    <DIR>          WebFolder
+03/06/2014  11:35    <DIR>          Widgets
+              13 fichier(s)        6ÿ558ÿ798 octets
+               6 R‚p(s)  690ÿ657ÿ497ÿ088 octets libres
---
+++ Untitled20/addedFromExplorer3.js
@@ -0,0 +1,27 @@
+ Le volume dans le lecteur C n'a pas de nom.
+ Le num‚ro de s‚rie du volume est 606E-C054
+
+ R‚pertoire de C:\Users\Ismail\Documents\Wakanda\Untitled20\Untitled20
+
+03/06/2014  14:42    <DIR>          .
+03/06/2014  14:42    <DIR>          ..
+03/06/2014  14:39             1ÿ182 addedFromExplorer.js
+03/06/2014  14:42             1ÿ241 addedFromExplorer2.js
+03/06/2014  14:42                 0 addedFromExplorer3.js
+03/06/2014  11:35    <DIR>          DataFolder
+17/04/2014  12:11               267 Model.waModel
+03/06/2014  12:21                21 newFile.js
+17/04/2014  12:11                68 Permissions.waPerm
+03/06/2014  11:35             1ÿ645 Settings.waSettings
+03/06/2014  14:41               344 SymbolTable.Match
+03/06/2014  14:41           197ÿ120 SymbolTable.waSym
+03/06/2014  14:42         6ÿ357ÿ504 SymbolTable.waSymData
+03/06/2014  11:35    <DIR>          Themes
+03/06/2014  12:21                40 toChange.js
+03/06/2014  12:25                15 toCheck.js
+03/06/2014  12:07                14 toDelete.js
+17/04/2014  12:11               578 Untitled20.waProject
+03/06/2014  11:35    <DIR>          WebFolder
+03/06/2014  11:35    <DIR>          Widgets
+              14 fichier(s)        6ÿ560ÿ039 octets
+               6 R‚p(s)  690ÿ657ÿ484ÿ800 octets libres
